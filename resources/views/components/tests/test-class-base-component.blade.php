504e1356ec030168539117bbf375815ae5b6b6e6
m.renya <renya.2000@icloud.com>
2021-07-25 19:44:55 +0900

    classBase

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,8 @@
+<div>
+
+    クラスベースのコンポーネントです
+    <div> {{$classBaseMessage}} </div>
+    <div> {{$defaultMessage}} </div>
+    <!-- The whole future lies in uncertainty: live immediately. - Seneca -->
+    
+</div>
