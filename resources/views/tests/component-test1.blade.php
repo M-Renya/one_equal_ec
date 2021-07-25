314f784fa406b233df287b9e6e67d2e03c893755
m.renya <renya.2000@icloud.com>
2021-07-25 18:42:17 +0900

    変数

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,6 +1,6 @@
 <x-tests.app>
     <x-slot name="header">ヘッダー1</x-slot>
-コンポーネントテスト１
+    コンポーネントテスト１
 
-    <x-tests.card title="タイトル1" content="本文2" />
+    <x-tests.card title="タイトル1" content="本文2" :message="$message" />
 </x-tests.app>
