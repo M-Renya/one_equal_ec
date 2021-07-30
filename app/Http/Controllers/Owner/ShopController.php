af69ecd615651adca5562b966d57787d9246384e
m.renya <renya.2000@icloud.com>
2021-07-30 21:41:09 +0900

    フォームリクエスト

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Models\Shop;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use InterventionImage;
+use App\Http\Requests\UploadImageRequest;
 
 
 class ShopController extends Controller
@@ -51,12 +53,30 @@ class ShopController extends Controller
         compact('shop'));
     }
 
-    public function update(Request $request, $id)
+    public function update(UploadImageRequest $request, $id)
     {
         $imageFile = $request->image; //一時保存
 
         if(!is_null($imageFile) && $imageFile->isValid() ){
-            Storage::putFile('public/shops', $imageFile);
+
+            // Storage::putFile('public/shops', $imageFile); //リサイズなし
+
+
+            // $fileName = uniqid(rand().'_');
+            // $extension = $imageFile->extension();;
+            // $fileNameToStore = $fileName. '.' . $extension;
+            // $resizedImage = InterventionImage::make($imageFile)
+            // ->resize(1920, 1080)
+            // ->encode();
+            $fileName = uniqid(rand().'_');
+            $extension = $imageFile->extension();
+            $fileNameToStore = $fileName. '.' . $extension;
+            $resizedImage = InterventionImage::make($imageFile)
+            ->resize(1920,1080)
+            ->encode();
+
+            Storage::put('public/shops/' . $fileNameToStore, $resizedImage );
+
         }
 
         return redirect()->route('owner.shops.index');
